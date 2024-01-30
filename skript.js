a5ead53b4dc17250358f76e6ee80a62830cd7715
AgniKai95 <97593111+AgniKai95@users.noreply.github.com>
2024-01-30 09:53:51 +0100

    Update

--- skript.js
+++ skript.js
@@ -319,7 +319,7 @@ WA.onInit()
       });
     }
 
-      currentPopup = WA.ui.openPopup("popUpStart","Willkommen beim Tag der Vernetzung - nutze die Chance und lerne die anderen Teilnehmenden kennen!\n\nAgenda:\n9:45-10:00 Vernetzungsspiel im Saal\n10:00 Impulsvortrag KI auf der Hauptbühne\nAb 10:30 Besuche die Sitzecke für verschiedene Gesprächsangebote, schnapp dir ein Team und nehm an unserer Rally teil oder besuche die Führung des Co-Working Bereiches! \n Darüber hinaus kannst du dich natürlich die ganze Zeit mit anderen Nachwuchskräften austauschen und vernetzen!\n Durch die Umgebung bewegst du dich mit den Pfeiltasten\n Viel Spaß!",[
+      currentPopup = WA.ui.openPopup("popUpStart","Willkommen beim Tag der Vernetzung - nutze die Chance und lerne die anderen Teilnehmenden kennen!\n\nAgenda:\n9:45-10:00 Vernetzungsspiel im Saal\n10:00 Impulsvortrag KI auf der Hauptbühne\nAb 10:30 Besuche die Sitzecke für verschiedene Gesprächsangebote, schnapp dir ein Team und nehm an unserer Rally teil oder besuche die Führung des Co-Working Bereiches! \n Darüber hinaus kannst du dich natürlich die ganze Zeit mit anderen Nachwuchskräften austauschen und vernetzen!\nDu hast ein technisches Problem? Lauf an unserem Helpdesk vorbei!\n Viel Spaß beim Vernetzen!",[
         {
             label: "Schließen",
             callback: (popup) => {
